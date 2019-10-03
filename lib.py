e89a010b4a337c5f79f31ec3f86320cb01bb6b2d
Edward Duffy, Arch Laptop <edward70891@gmail.com>
2019-10-03 18:16:04 +0100

    Add expansion support

--- lib.py
+++ lib.py
@@ -6,42 +6,53 @@ from PIL import Image, ImageDraw, ImageFont
 
 # Card options
 CARD_SCALE = 200
-BIG_TEXT_SCALE = 100
-TOP_TEXT_SCALE = 40
-TOP_TEXT = "Cult Against Humanity."
+CONTENT_TEXT_SCALE = 100
+TITLE_TEXT_SCALE = 40
+TITLE_TEXT = "Cult Against Humanity"
 # Margins (top big text, top small text, sides, bottom)
 MARGINS = (150, 50, 50, 50)
 # Chars to wrap at
 TEXT_WRAP = 17
+# otf/ttf file to use for the font
 FONT_TTF = "HelveticaNeueLTStd-Bd.otf"
+# Folders for the in-progress cards and built templates
 CARDS_DIR = "cards"
+BUILD_DIR = "build"
 
 # Program constants
+# Font
+CONTENT_FONT = ImageFont.truetype(FONT_TTF, size=CONTENT_TEXT_SCALE)
+CONTENT_TEXT_HEIGHT = math.floor(CONTENT_TEXT_SCALE * 1.2)
+TITLE_FONT = ImageFont.truetype(FONT_TTF, size=TITLE_TEXT_SCALE)
+TITLE_TEXT_HEIGHT = math.floor(TITLE_TEXT_SCALE * 1.2)
+# Other
 CARD_SIZE = (CARD_SCALE * 5, CARD_SCALE * 7)
-MAIN_FONT = ImageFont.truetype(FONT_TTF, size=BIG_TEXT_SCALE)
-SMALL_FONT = ImageFont.truetype(FONT_TTF, size=TOP_TEXT_SCALE)
 COLOURS = ("white", "black")
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-BIG_TEXT_HEIGHT = math.floor(BIG_TEXT_SCALE * 1.2)
-TOP_TEXT_HEIGHT = math.floor(TOP_TEXT_SCALE * 1.333)
+
+existing_folders = dict()
 
 
-def card_path(card_type, num, expansion=None):
-    if expansion is None:
-        folder = CARDS_DIR
-    else:
-        folder = os.path.join(CARDS_DIR, expansion.replace(" ", ""))
-    if not os.path.isdir(folder):
+def card_path(card_type, num, expansion=None, build=False):
+    global existing_folders
+    folder = BUILD_DIR if build else CARDS_DIR
+    if expansion is not None:
+        folder = os.path.join(folder, expansion.replace(" ", ""))
+    folder = os.path.join(folder, card_type)
+    if not (existing_folders.get(folder) or os.path.isdir(folder)):
         os.makedirs(folder)
-    return os.path.join(folder, f"{card_type}{num}.png")
+        existing_folders[folder] = True
+    return os.path.join(folder, f"card{num}.png")
 
 
-def make_card(card_text,
-              file_name,
-              expansion="",
-              card_type="white",
-              show_small=True):
+def make_card(
+        card_text,
+        file_name,
+        expansion="",
+        card_type="white",
+        show_small=True,
+):
     if card_type == "white":
         text_col = BLACK
         back_col = WHITE
@@ -63,11 +74,11 @@ def make_card(card_text,
 
     # Add the main text
     for num, line in enumerate(card_text):
-        pos = (MARGINS[2], MARGINS[0] + (BIG_TEXT_HEIGHT * num))
+        pos = (MARGINS[2], MARGINS[0] + (CONTENT_TEXT_HEIGHT * num))
         drawn.text(
             pos,
             line,
-            font=MAIN_FONT,
+            font=CONTENT_FONT,
             fill=text_col,
         )
 
@@ -75,16 +86,16 @@ def make_card(card_text,
         # Add the header text
         drawn.text(
             (MARGINS[2], MARGINS[1]),
-            "Cult Against Humanity",
-            font=SMALL_FONT,
+            TITLE_TEXT,
+            font=TITLE_FONT,
             fill=text_col,
         )
 
         # Add the footer text
         drawn.text(
-            (MARGINS[2], CARD_SIZE[1] - MARGINS[3] - TOP_TEXT_HEIGHT),
+            (MARGINS[2], CARD_SIZE[1] - MARGINS[3] - TITLE_TEXT_HEIGHT),
             expansion,
-            font=SMALL_FONT,
+            font=TITLE_FONT,
             fill=text_col,
         )
 
--- make_cards.py
+++ make_cards.py
@@ -1,44 +1,59 @@
 #!/usr/bin/env python
 import csv
+import shutil
 from concurrent import futures
 from itertools import zip_longest
 from urllib import request
 
 import psutil
 
-from lib import COLOURS, TOP_TEXT, card_path, make_card
+from lib import BUILD_DIR, CARDS_DIR, COLOURS, TITLE_TEXT, card_path, make_card
 
 SPREADSHEET_ID = open("spreadsheet_id.txt", "rt").readline()
 CSV_FILE = "raw.csv"
+EXPANSIONS = (
+    "Base_Game",
+    "Apex_Expansion",
+    "Danganronpa_Expansion",
+    "FragSoc_Expansion",
+    "Misc_Games_Expansion",
+    "Overwatch_Expansion",
+    "TF2_Expansion",
+    "Titanfall_Expansion",
+)
 
-# Get our csv and decode into string
-csv_raw = request.urlopen(
-    f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}/export?exportFormat=csv"
-).read().decode("utf-8")
-csv_reader = csv.reader(csv_raw.split("\n"))
+# Clear results directories
+shutil.rmtree(BUILD_DIR)
+shutil.rmtree(CARDS_DIR)
 
-# Write the csv data to a file for debugging
-with open(CSV_FILE, "wt") as my_file:
-    my_file.write(csv_raw)
+for expansion in EXPANSIONS:
+    # Get our csv, decode into string and make a reader
+    csv_raw = request.urlopen(
+        f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}/gviz/tq?tqx=out:csv&sheet={expansion}"
+    ).read().decode("utf-8")
+    csv_reader = csv.reader(csv_raw.split("\n"))
 
-# Format and Write the cards
-expansion = "Base Game"  # Temp var
-for colour, cards in zip(COLOURS, zip_longest(*csv_reader)):
-    with futures.ThreadPoolExecutor(psutil.cpu_count()) as executor:
-        executor.map(
-            lambda elem: make_card(*elem),
-            [(
-                c[1],
-                card_path(colour, c[0], expansion=expansion),
-                expansion,
-                colour,
-            ) for c in enumerate(cards)],
-        )
+    # Write the csv data to a file for debugging
+    with open(CSV_FILE, "wt") as my_file:
+        my_file.write(csv_raw)
+
+    # Format and Write the cards
+    for colour, cards in zip(COLOURS, zip_longest(*csv_reader)):
+        with futures.ThreadPoolExecutor(psutil.cpu_count()) as executor:
+            executor.map(
+                lambda elem: make_card(*elem),
+                [(
+                    c[1],
+                    card_path(colour, c[0], expansion=expansion),
+                    expansion.replace("_", " "),
+                    colour,
+                ) for c in enumerate(cards)],
+            )
 
 # Create card backs
 for colour in COLOURS:
     make_card(
-        TOP_TEXT + ".",
+        TITLE_TEXT + ".",
         card_path(colour, "Back"),
         show_small=False,
         card_type=colour,
