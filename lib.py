650d409e1bfa2154e832056c3c7de6b00882cf42
Edward Duffy, Arch Laptop <edward70891@gmail.com>
2019-10-03 03:57:51 +0100

    Add functionality to make_card

--- lib.py
+++ lib.py
@@ -1,3 +1,4 @@
+import enum
 import math
 import textwrap
 
@@ -6,12 +7,13 @@ from PIL import Image, ImageDraw, ImageFont
 # Card options
 CARD_SCALE = 200
 BIG_TEXT_SCALE = 120
-BIG_TEXT_HEIGHT = math.floor(BIG_TEXT_SCALE * 1.333)
-BIG_TEXT_SPACING_VERTICAL = math.floor(BIG_TEXT_HEIGHT / 10)
 TOP_TEXT_SCALE = 40
 TOP_TEXT = "Cult Against Humanity."
 MARGIN_SIDE = 50
-MARGIN_TOP = (150, 50)  # (big, small)
+MARGIN_BOTTOM = 50
+# Margins at the top (margin for large text, margin for header)
+MARGIN_TOP = (150, 50)
+# Chars to wrap at
 TEXT_WRAP = 14
 FONT_TTF = "HelveticaNeueLTStd-Bd.otf"
 
@@ -22,9 +24,28 @@ MAIN_FONT = ImageFont.truetype(FONT_TTF, size=BIG_TEXT_SCALE)
 SMALL_FONT = ImageFont.truetype(FONT_TTF, size=TOP_TEXT_SCALE)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+BIG_TEXT_HEIGHT = math.floor(BIG_TEXT_SCALE * 1.333)
+BIG_TEXT_SPACING_VERTICAL = math.floor(BIG_TEXT_HEIGHT / 20)
+TOP_TEXT_HEIGHT = math.floor(TOP_TEXT_SCALE * 1.333)
 
 
-def make_card(card_text, file_name):
+class CARD_TYPE(enum.Enum):
+    WHITE = 0
+    BLACK = 1
+
+
+def make_card(card_text,
+              file_name,
+              expansion="",
+              card_type=CARD_TYPE.WHITE,
+              show_small=True):
+    if card_type == CARD_TYPE.WHITE:
+        text_col = BLACK
+        back_col = WHITE
+    else:
+        text_col = WHITE
+        back_col = BLACK
+
     # Skip blank lines created by fucked formatting
     if card_text in ([""], []):
         return
@@ -34,16 +55,8 @@ def make_card(card_text, file_name):
     card_text = textwrap.wrap(card_text, width=TEXT_WRAP)
 
     # Initialise image
-    current_img = Image.new("RGB", (1000, 1600), color=WHITE)
+    current_img = Image.new("RGB", (1000, 1600), color=back_col)
     drawn = ImageDraw.Draw(current_img)
-
-    # Add the header text
-    drawn.text(
-        (MARGIN_SIDE, MARGIN_TOP[1]),
-        "Cult Against Humanity",
-        font=SMALL_FONT,
-        fill=BLACK,
-    )
 
     # Add the main text
     for num, line in enumerate(card_text):
@@ -53,7 +66,27 @@ def make_card(card_text, file_name):
             pos,
             line,
             font=MAIN_FONT,
-            fill=BLACK,
+            fill=text_col,
+        )
+
+    if show_small is True:
+        # Add the header text
+        drawn.text(
+            (MARGIN_SIDE, MARGIN_TOP[1]),
+            "Cult Against Humanity",
+            font=SMALL_FONT,
+            fill=text_col,
+        )
+
+        # Add the footer text
+        drawn.text(
+            (
+                MARGIN_SIDE,
+                CARD_SIZE[1] - (TOP_TEXT_HEIGHT + MARGIN_BOTTOM),
+            ),
+            expansion,
+            font=SMALL_FONT,
+            fill=text_col,
         )
 
     # Save it
--- make_cards.py
+++ make_cards.py
@@ -6,10 +6,10 @@ from urllib import request
 
 import psutil
 
-from lib import make_card
+from lib import CARD_TYPE, make_card
 
-CSV_NAME = "values.csv"
 GDOC_ID = open("spreadsheet_id.txt", "rt").readline()
+CARDS_DIR = "cards"
 
 # Get our csv and decode into string
 csv_raw = request.urlopen(
@@ -22,6 +22,17 @@ with futures.ThreadPoolExecutor(psutil.cpu_count()) as executor:
         lambda elem: make_card(
             elem[1],
             os.path.join("cards", f"card{elem[0]}.png"),
+            expansion="Base Game",
         ),
         enumerate(csv.reader(csv_raw.split("\n"))),
     )
+
+# Create card backs
+make_card("Cult Against Humanity.",
+          os.path.join(CARDS_DIR, "backBlack.png"),
+          show_small=False,
+          card_type=CARD_TYPE.BLACK)
+make_card("Cult Against Humanity.",
+          os.path.join(CARDS_DIR, "backWhite.png"),
+          show_small=False,
+          card_type=CARD_TYPE.WHITE)
